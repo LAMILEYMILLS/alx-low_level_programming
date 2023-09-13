f929a67719800842828beca840cff66a99fd7ce2
lamileymills <keaziahmills3@gmail.com>
2023-09-13 06:22:57 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
- /**
-  * print_alphabet - function to print alphabet in lowercase
-  *
-  */
+/**
+* print_alphabet - function to print alphabet in lower case
+* Return: 0
+*/
 
 void print_alphabet(void)
 {
